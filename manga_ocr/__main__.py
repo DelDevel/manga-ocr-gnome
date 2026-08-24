4d0f6bb9ae5ee468dc73242137d17269f74b00e9
Guilherme do Vale <deldeveloper@proton.me>
2026-08-24 18:12:03 -0300

    Update __main__.py

--- manga_ocr/__main__.py
+++ manga_ocr/__main__.py
@@ -1,12 +1,10 @@
 import fire
 
-# Remove the top-level direct import of 'run'
 import manga_ocr.run
 from .patches import apply_patches
 
 def main():
     apply_patches()
-    # Reference the run function directly through the module namespace
     fire.Fire(manga_ocr.run.run)
 
 if __name__ == "__main__":
